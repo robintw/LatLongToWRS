260e2189b58f7eecb0ade4993e3c8bab991cacd1
Robin Wilson <robin@rtwilson.com>
2013-03-03 15:46:21 +0000

    Added more comments on implementation & Added README.

--- get_wrs.py
+++ get_wrs.py
@@ -41,17 +41,28 @@ class ConvertToWRS:
         using the shapefile keyword - but it should work if the
         shapefile is in the same directory.
         """
+        # Open the shapefile
         self.shapefile = ogr.Open(shapefile)
+        # Get the only layer within it
         self.layer = self.shapefile.GetLayer(0)
 
         self.polygons = []
 
+        # For each feature in the layer
         for i in range(self.layer.GetFeatureCount()):
+            # Get the feature, and its path and row attributes
             feature = self.layer.GetFeature(i)
             path = feature['PATH']
             row = feature['ROW']
+
+            # Get the geometry into a Shapely-compatible
+            # format by converting to Well-known Text (Wkt)
+            # and importing that into shapely
             geom = feature.GetGeometryRef()
             shape = shapely.wkt.loads(geom.ExportToWkt())
+
+            # Store the shape and the path/row values
+            # in a list so we can search it easily later
             self.polygons.append((shape, path, row))
 
 
@@ -65,10 +76,18 @@ class ConvertToWRS:
         [{path: 202, row: 26}, {path:186, row=7}]
         """
 
+        # Create a point with the given latitude
+        # and longitude (NB: the arguments are lon, lat
+        # not lat, lon)
         pt = shapely.geometry.Point(lon, lat)
         res = []
+        # Iterate through every polgon
         for poly in self.polygons:
+            # If the point is within the polygon then
+            # append the current path/row to the results
+            # list
             if pt.within(poly[0]):
                 res.append({'path': poly[1], 'row': poly[2]})
 
+        # Return the results list to the user
         return res
